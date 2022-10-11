9efd027374e98baefc8eb29f01c861ce9ac8e006
Andrey Antukh <niwi@niwi.nz>
2022-10-11 14:53:08 +0200

    :sparkles: Do not return the whole file on file rename

--- backend/src/app/rpc/mutations/files.clj
+++ backend/src/app/rpc/mutations/files.clj
@@ -118,10 +118,11 @@
 
 (defn- rename-file
   [conn {:keys [id name] :as params}]
-  (db/update! conn :file
-              {:name name}
-              {:id id}))
-
+  (-> (db/update! conn :file
+                  {:name name
+                   :modified-at (dt/now)}
+                  {:id id})
+      (select-keys [:id :name :created-at :modified-at])))
 
 ;; --- Mutation: Set File shared
 
